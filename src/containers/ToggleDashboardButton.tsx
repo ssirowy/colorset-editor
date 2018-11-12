1c80830f100f4d5e3748ac83ae2c6b3b22246fb9
Scott Sirowy <scott.sirowy@looker.com>
2018-11-12 15:58:16 -0800

    Disable if no selected collection

--- src/containers/ToggleDashboardButton.tsx
+++ src/containers/ToggleDashboardButton.tsx
@@ -2,11 +2,16 @@ import { Button } from 'looker-lens'
 import { connect } from 'react-redux'
 import { showDashboard } from '../store/actions'
 
-// Dispatch props
+import { ApplicationState } from '../store'
+
 const mapDispatchToProps = (dispatch: any) => ({
   onClick: () => {
     dispatch(showDashboard(false))
   },
 })
+
+const mapStateToProps = (state: ApplicationState) => ({
+  disabled: state.selectedCollection.length === 0
+})
 ​
-export const ToggleDashboardButton = connect(null,mapDispatchToProps)(Button)
+export const ToggleDashboardButton = connect(mapStateToProps , mapDispatchToProps)(Button)
